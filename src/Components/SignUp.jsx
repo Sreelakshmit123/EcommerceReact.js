4a332b81f08c17e8cbaa537b65f7b4e4c114f6ec
Sreelakshmit123 <tsreelakshmi88@gmail.com>
2025-08-01 09:21:34 +0530

    upadte

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -218,7 +218,7 @@ function SignUp() {
                                     <Button onClick={handleSubmitSignUp} className="sign-up-button w-100 pt-3 pb-3">
                                         Sign Up
                                     </Button>
-                                    <div className='text-center small mt-1'>Have an Account ? <Link to={'/login'}>Login Here</Link></div>
+                                    <div className='text-center text-secondary fw-light small mt-1'>Have an Account ? <Link to={'/login'}>Login Here</Link></div>
                                     <div className="d-flex justify-content-evenly align-items-center mt-1 text-muted small ">
                                         <a href="#" className='text-dark text-decoration-none'>Customer Support</a>
                                         <a href="#" className='text-dark text-decoration-none'>Terms of Service</a>
